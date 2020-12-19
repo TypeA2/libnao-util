63501267d7d54f1db0f1e863b0a685cb2954ebcc
TypeA2 <nuanlim29@gmail.com>
2020-12-19 19:29:39 +0100

    initial commit

---
+++ libnao-util.cpp
@@ -0,0 +1,12 @@
+﻿// libnao-util.cpp : Defines the entry point for the application.
+//
+
+#include "libnao-util.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
